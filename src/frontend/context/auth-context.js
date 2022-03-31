59803c7cc7698c2ad27a08a88b5f36051399c5a3
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-03-31 12:34:11 +0530

    feat : created auth context

---
+++ src/frontend/context/auth-context.js
@@ -0,0 +1,24 @@
+import React, { useState, useContext, createContext } from "react"
+
+const AuthContext = createContext();
+
+
+const AuthProvider = ({ children }) => {
+
+    const [auth, setAuth] = useState({
+        status: localStorage.getItem("AUTH_TOKEN") ? true : false,
+        token: localStorage.getItem("AUTH_TOKEN"),
+        userName:"No Account",
+    });
+
+
+    return (
+        <AuthContext.Provider value={{ auth, setAuth }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
