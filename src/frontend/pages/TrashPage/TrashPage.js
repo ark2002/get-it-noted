f0b8ee97affc56751240856a2ca2eac6ca9c1082
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-01 13:07:30 +0530

    feat : added main-pages
    - added notes page
    - added labels page
    - added archives page
    - added trash page

---
+++ src/frontend/pages/TrashPage/TrashPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TrashPage = () => {
+
+    return (
+        <>
+            TrashPage
+        </>
+    );
+}
+
+export { TrashPage };
--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -2,3 +2,6 @@ export { LandingPage } from "./LandingPage/LandingPage";
 export { NotesPage } from "./NotesPage/NotesPage";
 export { SignInPage } from "./SignInPage/SignInPage";
 export { SignUpPage } from "./SignUpPage/SignUpPage";
+export { TrashPage } from "./TrashPage/TrashPage";
+export { ArchivesPage } from "./ArchivesPage/ArchivesPage";
+export { LabelsPage } from "./LabelsPage/LabelsPage";
