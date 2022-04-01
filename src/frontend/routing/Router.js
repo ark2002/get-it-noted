20ac177b9503bc7c059db79c3a037483b99aca5a
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-01 13:08:18 +0530

    feat : new routes added and private routing

--- src/frontend/routing/Router.js
+++ src/frontend/routing/Router.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LandingPage, NotesPage, SignInPage, SignUpPage } from "../pages";
+
+import { ArchivesPage, LabelsPage, LandingPage, NotesPage, SignInPage, SignUpPage, TrashPage } from "../pages";
+import { PrivateRoute } from "../components";
+import { useAuth } from "../context"
 
 const Router = () => {
-
+    const { auth: { status }, } = useAuth();
     return (
         <>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
-                <Route path="/notes" element={<NotesPage />} />
-                <Route path="/signin" element={<SignInPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/notes" element={<PrivateRoute><NotesPage /></PrivateRoute>} />
+                <Route path="/labels" element={<PrivateRoute><LabelsPage /></PrivateRoute>} />
+                <Route path="/archives" element={<PrivateRoute><ArchivesPage /></PrivateRoute>} />
+                <Route path="/trash" element={<PrivateRoute><TrashPage /></PrivateRoute>} />
+                {!status && (
+                    <>
+                        <Route path="/signin" element={<SignInPage />} />
+                        <Route path="/signup" element={<SignUpPage />} />
+                    </>
+                )}
                 <Route path="*" element={<Navigate replace to="/" />} />
             </Routes>
         </>
