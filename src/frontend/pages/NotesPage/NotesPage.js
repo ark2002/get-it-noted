7525ffbdae722e232c6e09e05da072ff94a40208
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-03 17:56:27 +0530

    feat : connected everything 🤩

--- src/frontend/pages/NotesPage/NotesPage.js
+++ src/frontend/pages/NotesPage/NotesPage.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { EditableNote } from "../../components";
+import { BasicNoteCard, EditableNote, NotesList } from "../../components";
 
 import "./NotesPage.css"
 
 const NotesPage = () => {
     return (
         <>
-            <div className="notespage__container flex--row">
-                <EditableNote />
+            <div className="notespage__container">
+                <div className="notes__list flex--column">
+                    <EditableNote />
+                    <NotesList />
+                </div>
             </div>
         </>
     );
