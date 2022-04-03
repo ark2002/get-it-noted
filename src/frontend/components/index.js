f7bc05905d2a06e6bfb2b75c0607807f5532e5b2
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-03 17:55:02 +0530

    feat: created basic note component

--- src/frontend/components/index.js
+++ src/frontend/components/index.js
@@ -4,3 +4,5 @@ export { Sidebar } from "./Sidebar/Sidebar";
 export { PrivateRoute } from "./PrivateRoute/PrivateRoute"
 export { RichTextEditor } from "./RichTextEditor/RichTextEditor"
 export { EditableNote } from "./EditableNote/EditabaleNote"
+export { BasicNoteCard } from "./BasicNoteCard/BasicNoteCard"
+export { NotesList } from "./NotesList/NotesList"
