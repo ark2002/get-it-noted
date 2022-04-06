5d781f1c09d2dfb39fdcbdcaa59488009b8ac4db
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-06 22:50:19 +0530

    feat: trash page edits

--- src/frontend/pages/TrashPage/TrashPage.js
+++ src/frontend/pages/TrashPage/TrashPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { TrashNote } from "../../components";
 import { useNotes } from "../../context";
+import "./TrashPage.css"
 
 const TrashPage = () => {
 
@@ -9,14 +10,14 @@ const TrashPage = () => {
     const trashedNotes = notes.filter((note) => note.trash);
 
     return (
-        <>
+        <div className="trash__page">
             {trashedNotes.length > 0 ? <div className="trashed-list flex--column">
                 <h1 className="primary__font trashed-list__title">Trash Notes</h1>
                 {trashedNotes.map((note) => <TrashNote key={note._id} note={note} setNotes={setNotes} />)}
             </div>:<div>
             <h1 className="primary__font pinned-list__title">Trashed Notes Will Be Visible Here</h1>
                 </div>}
-        </>
+        </div>
     );
 }
 
