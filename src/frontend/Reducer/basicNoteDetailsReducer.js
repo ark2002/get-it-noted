26dde4aebecd33eb240f49f1acf20d2f4abff1ed
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-07-18 19:02:53 +0530

    refac : Filters

--- src/frontend/Reducer/basicNoteDetailsReducer.js
+++ src/frontend/Reducer/basicNoteDetailsReducer.js
@@ -1,23 +1,30 @@
 import React from "react";
 
 export const basicNoteDetailsReducer = (state, action) => {
-    const { type, payload } = action;
-    switch (type) {
-        case "COLORLIST":
-            return { ...state, colorlist: payload };
-        case "LABELLIST":
-            return { ...state, labellist: payload };
-        case "TITLETOGGLE":
-            return { ...state, titleToggle: payload };
-        case "CONTENTTOGGLE":
-            return { ...state, contentToggle: payload };
-        case "PRIORITYLIST":
-            return { ...state, prioritylist: payload };
-        case "NEWTITLE":
-            return { ...state, newTitle: payload };
-        case "NEWCONTENT":
-            return { ...state, newContent: payload };
-        default:
-            return state;
-    }
-}
+  const { type, payload } = action;
+  switch (type) {
+    case "COLORLIST":
+      return { ...state, colorlist: payload };
+    case "LABELLIST":
+      return { ...state, labellist: payload };
+    case "TITLETOGGLE":
+      return { ...state, titleToggle: payload };
+    case "CONTENTTOGGLE":
+      return { ...state, contentToggle: payload };
+    case "PRIORITYLIST":
+      return { ...state, prioritylist: payload };
+    case "NEWTITLE":
+      return { ...state, newTitle: payload };
+    case "NEWCONTENT":
+      return { ...state, newContent: payload };
+    case "CLOSEALL":
+      return {
+        ...state,
+        colorlist: false,
+        labellist: false,
+        prioritylist: false,
+      };
+    default:
+      return state;
+  }
+};
